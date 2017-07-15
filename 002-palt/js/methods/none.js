fccc70d07ba3254bf6c0d88ee281935d9875f65c
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-07-15 18:52:20 +0530

    add stub method

---
+++ 002-palt/js/methods/none.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * A stub implementation, in order to check parsing speed.
+ * @param {Pixel[]} image 
+ * @param {Pixel[]} palette 
+ */
+function none(image, palette) {
+  return [];
+}
+
+module.exports = none;
