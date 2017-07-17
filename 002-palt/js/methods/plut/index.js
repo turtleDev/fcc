bfff6ae41c8c6770c133256393ab3c9d84388502
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-07-17 20:24:11 +0530

    optimize(?) plut method

--- 002-palt/js/methods/plut/index.js
+++ 002-palt/js/methods/plut/index.js
@@ -22,20 +22,44 @@ class PlutSearch {
 
   run(image) {
     const result = Array(image.length);
-    let pixel;
+    let pixel, other;
     let rg, rb, gb;
     let d1, d2, d3;
+    let tmp;
     const { RGLut, RBLut, GBLut } = this; 
     const { palette } = this;
+    const { sqrt, pow, abs } = Math;
     for ( let i = 0; i < image.length; ++i ) {
       pixel = image[i];
       rg = RGLut[pixel.red][pixel.green];
       rb = RBLut[pixel.red][pixel.blue];
       gb = GBLut[pixel.green][pixel.blue];
 
-      d1 = palette[rg].distance(pixel);
-      d2 = palette[rb].distance(pixel);
-      d3 = palette[gb].distance(pixel);
+      // d1 = palette[rg].distance(pixel);
+      // d2 = palette[rb].distance(pixel);
+      // d3 = palette[gb].distance(pixel);
+
+      // inline
+      tmp = palette[rg];
+      d1 = sqrt(
+        Math.pow(Math.abs(tmp.red - pixel.red), 2) +
+        Math.pow(Math.abs(tmp.green - pixel.green), 2) +
+        Math.pow(Math.abs(tmp.blue - pixel.blue), 2)
+      );
+
+      tmp = palette[rb];
+      d2 = sqrt(
+        Math.pow(Math.abs(tmp.red - pixel.red), 2) +
+        Math.pow(Math.abs(tmp.green - pixel.green), 2) +
+        Math.pow(Math.abs(tmp.blue - pixel.blue), 2)
+      );
+
+      tmp = palette[gb];
+      d3 = sqrt(
+        Math.pow(Math.abs(tmp.red - pixel.red), 2) +
+        Math.pow(Math.abs(tmp.green - pixel.green), 2) +
+        Math.pow(Math.abs(tmp.blue - pixel.blue), 2)
+      );
 
       if ( d1 < d2 && d1 < d3 ) {
         result[i] = rg;
