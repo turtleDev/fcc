9302624ce985b1d5c5edfeb9919010ab53e5f2d7
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-07-17 20:27:35 +0530

    update none impl

--- 002-palt/js/methods/none.js
+++ 002-palt/js/methods/none.js
@@ -4,13 +4,18 @@ const Pixel = require('../pixel');
 
 /**
  * A stub implementation.
- * useful for checking parsing speeds.
+ * useful for setting a benchmark on the upperlimit
+ * for runtime
  * @class none
  */
 class none {
   init() {}
   run(image) {
-    return [];
+    const results = Array(image.length);
+    for ( let i = 0; i < results.length; ++i ) {
+      results[i] = Math.floor(image.length * Math.random());
+    }
+    return results;
   }
 }
 
