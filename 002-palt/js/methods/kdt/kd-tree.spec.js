88daa30601a523e7e37c94da9534dbe84bd76519
Saravjeet 'Aman' Singh <saravjeetamansingh@gmail.com>
2017-07-18 11:44:35 +0530

    add new test to kd-tree method

--- 002-palt/js/methods/kdt/kd-tree.spec.js
+++ 002-palt/js/methods/kdt/kd-tree.spec.js
@@ -26,8 +26,54 @@ const Pixel = require('../../pixel');
   ]
   const points = dataset.slice();
   const kdt = new KDTree(points, ['red', 'green', 'blue']);
-  const nn = kdt.findNN(new Pixel(111, 111, 111))
-  if ( nn != dataset[1] ) {
+  const nn = kdt.findNN(new Pixel(2, 2, 2))
+  if ( nn != dataset[2] ) {
+    throw new Error('failed');
+  }
+})();
+
+(function evenLessSimpleTest() {
+  const p = (v) => new Pixel(v, v ,v);
+  const dataset = [
+    p(1),
+    p(3),
+    p(4),
+    p(3),
+    p(123),
+    p(4),
+    p(12),
+    p(1),
+    p(23),
+    p(4),
+    p(1),
+    p(223),
+    p(4),
+    p(3),
+    p(41),
+    p(23),
+    p(123),
+    p(42),
+    p(213),
+    p(32),
+    p(23),
+    p(32),
+    p(4),
+    p(54),
+    p(65),
+    p(3),
+    p(23),
+    p(43),
+    p(47),
+    p(65)
+  ];
+
+  const points = dataset.slice();
+  const kdt = new KDTree(points, ['red', 'green', 'blue']);
+  const idx = Math.floor(dataset.length * Math.random());
+  const result = kdt.findNN(dataset[idx]);
+  if ( JSON.stringify(dataset[idx]) != JSON.stringify(result) ) {
+    console.log('original pixel', dataset[idx]);
+    console.log('pixel found', result);
     throw new Error('failed');
   }
 })();
